26e7ef7f0ddf469802afb61f8b6ad626b717ca6c
Matthew Fortunka <fortunkam@MININT-9RKAFT4.europe.corp.microsoft.com>
2020-09-14 14:48:14 +0100

    make test fail

--- apis/trips/tripsgo/healthCheckService.go
+++ apis/trips/tripsgo/healthCheckService.go
@@ -7,7 +7,7 @@ import (
 
 func healthcheckGet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusInternalServerError)
 
 	hc := &Healthcheck{Message: "Trip Service Healthcheck", Status: "Healthy"}
 
